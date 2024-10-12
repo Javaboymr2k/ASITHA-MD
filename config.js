8b1ab9abae83ea799eecd612e8e4fbabfb7f4031
Javaboymr2k <javaboymr2k@gmail.com>
2024-10-12 09:49:50 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=f7plQRAK#IVbIQQ5mqgevlbNSHlOyWlgyBgfR8Ik5ZRaoYS00ygg",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=Pupk1RAD#P4-sSbqOy8fJ9kTYnMdhgPNCoklCodpeLT5PP1Tj56Q",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
